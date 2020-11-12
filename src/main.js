17bebc82ef40ab0a27995c6ac948c85d2a4a799d
shanlin.tu <shanlin.tu@cisdi.com.cn>
2020-11-12 17:23:49 +0800

    初始

--- src/main.js
+++ src/main.js
@@ -1,4 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import Antd from 'ant-design-vue';
+import 'ant-design-vue/dist/antd.css';
 
-createApp(App).mount('#app')
+const app = createApp();
+app.config.productionTip = false;
+
+
+createApp(App).use(Antd);
